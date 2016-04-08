46ab7c5fbf5825e66a984f8886b80f8265cf0f4a
Alexander <ronaldo173@inbox.ru>
2016-04-08 12:10:23 +0300

    Initial commit

---
+++ src/ua/ronaldo173/library/web/beans/User.java
@@ -0,0 +1,27 @@
+package ua.ronaldo173.library.web.beans;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class User implements Serializable{
+
+    private String username;
+    
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    
+    
+    
+}
